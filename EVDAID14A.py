e1fee9771bbab50f476d966abd51cd519953e038
Naveenbadarla <naveen.badarla@eon.com>
2025-11-30 16:06:57 +0100

    Update EVDAID14A.py

--- EVDAID14A.py
+++ EVDAID14A.py
@@ -57,7 +57,7 @@ st.markdown("""
 
         /* Logo animation */
         .eon-header-logo {
-            width: 95px;
+            width: 130px;
             height: auto;
             margin-right: 25px;
             animation: logoGlow 4s ease-in-out infinite;
